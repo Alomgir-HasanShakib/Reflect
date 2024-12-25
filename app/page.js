21b475eb2df561e91de4cfd466bc1df8b4598933
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-12-25 18:10:13 +0600

    completed call to action section

--- app/page.js
+++ app/page.js
@@ -219,6 +219,29 @@ export default function Home() {
         </div>
 
         {/* ********************************************FAQ section end ********************************************** */}
+
+        {/* ********************************************CTA section Start ********************************************** */}
+
+        <div className="mt-24">
+          <Card className="bg-gradient-to-r from-orange-100 to-amber-100">
+            <CardContent className="p-12 text-center">
+              <h2 className="text-3xl font-bold text-orange-900 mb-6">
+                Start Reflct-ing on Your Journey Today!
+              </h2>
+              <p className="text-lg text-orange-700 mb-8 max-w-2xl mx-auto">
+                Join thousands of writers who have already discovered the power
+                of digital journaling.
+              </p>
+              <Link href={"/dashboard"}>
+                <Button variant="journal" size="lg" className="animate-bounce">
+                  Get Started For Free <ChevronRight className="ml-2 h-4 w-4"/>
+                </Button>
+              </Link>
+            </CardContent>
+          </Card>
+        </div>
+
+        {/* ********************************************CTA section end ********************************************** */}
       </div>
     </>
   );
