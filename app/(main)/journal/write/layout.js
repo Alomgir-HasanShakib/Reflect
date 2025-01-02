a8a6ee48e0d64d8e67b5aff4f56c53f86bdb7db8
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-02 11:37:12 +0600

    created write journal layout

---
+++ app/(main)/journal/write/layout.js
@@ -0,0 +1,24 @@
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import React, { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+
+const WriteLayout = ({ children }) => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div>
+        <Link
+          href={"/dashboard"}
+          className="flex items-center gap-2 text-sm text-orange-600 hover:text-orange-700 cursor-pointer"
+        >
+          <ArrowLeft /> Back to Dashboard
+        </Link>
+      </div>
+      <Suspense fallback={<BarLoader color="orange" width={"100%"} />}>
+        {children}
+      </Suspense>
+    </div>
+  );
+};
+
+export default WriteLayout;
